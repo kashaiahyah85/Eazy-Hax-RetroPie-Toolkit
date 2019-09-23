13bdc98d8515aaa32c6fe71405a29901181cec71
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-09-23 10:28:26 -0700

    modified:   Update Toolkit.sh

--- Eazy Hax Toolkit/Update Toolkit.sh
+++ Eazy Hax Toolkit/Update Toolkit.sh
@@ -2,15 +2,15 @@
 if [ ! -f /usr/bin/svn ]; then
 	echo "Subversion (the tool used to manage updates) is missing. Installing it now"
 	sleep 5
-	sudo apt-get install -y subversion
+	sudo apt-get install -yqq subversion
 fi
 
-cd ~/RetroPie/retropiemenu/Eazy\ Hax\ Toolkit/
+cd ~/RetroPie/retropiemenu/Eazy\ Hax\ Toolkit/ || exit
 svn up --accept tf
 
-REV=`svn info | grep "Revision" | awk '{print $2}'`
-DATE=`svn info | grep "Last Changed Date" |awk '{print $4}'`
-TIME=`svn info | grep "Last Changed Date" |awk '{print $5}'`
+REV= $(svn info | grep "Revision" | awk '{print $2}')
+DATE= $(`svn info | grep "Last Changed Date" |awk '{print $4}')
+TIME= $(`svn info | grep "Last Changed Date" |awk '{print $5}')
 echo "You have been upgraded to the latest version $REV"
 echo "Last update was on $DATE at $TIME"
 echo "Rebooting your system shortly so that the changes take effect, and or new scripts show up."
